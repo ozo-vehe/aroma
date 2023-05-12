45642504e2594ab3ad500ffdfda57213e76f9983
ozo-vehe <johnnie.vehe@gmail.com>
2023-05-12 23:55:14 +0100

    finished produc section component

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -6,22 +6,22 @@
 </script>
 
 <template>
-  <section class="group product border border-black w-250">
-    <div class="relative border border-green-400 overflow-hidden">
+  <section class="group product w-250 mb-8">
+    <div class="relative">
       <img class="w-full" :src="product.image" :alt="product.name">
-      <div class="absolute bottom-0 z-10 bg-white/90 w-full h-32 translate-y-16 opacity-0 group-hover:translate-y-8 group-hover:opacity-100 transition-all duration-500">
-        <div class="absolute flex gap-x-2 left-0 z-10">
-          <img class="icon p-2 w-8 h-8 bg-custom-blue/80 hover:bg-custom-blue duration-350 transition-all rounded" src="https://img.icons8.com/ios/50/null/search--v1.png" alt="search">
-          <img class="icon p-2 w-8 h-8 bg-custom-blue/80 hover:bg-custom-blue duration-350 transition-all rounded" src="https://img.icons8.com/material-outlined/24/null/shopping-cart--v1.png" alt="shopping-cart">
-          <img class="icon p-2 w-8 h-8 bg-custom-blue/80 hover:bg-custom-blue duration-350 transition-all rounded" src="https://img.icons8.com/ios/50/null/hearts--v1.png" alt="favourite"/>
+      <div class="z-50 flex items-center justify-center absolute bottom-0 bg-white/70 w-full h-24 translate-y-16 opacity-0 group-hover:translate-y-0 group-hover:opacity-100 transition-all duration-500">
+        <div class="absolute flex gap-x-4 z-10">
+          <img class="icon p-3 w-10 h-10 bg-custom-blue/80 hover:bg-custom-blue duration-350 transition-all" src="https://img.icons8.com/ios/50/ffffff/search--v1.png" alt="search">
+          <img class="icon p-3 w-10 h-10 bg-custom-blue/80 hover:bg-custom-blue duration-350 transition-all" src="https://img.icons8.com/material-outlined/24/ffffff/shopping-cart--v1.png" alt="shopping-cart">
+          <img class="icon p-3 w-10 h-10 bg-custom-blue/80 hover:bg-custom-blue duration-350 transition-all" src="https://img.icons8.com/ios/50/ffffff/hearts--v1.png" alt="favourite"/>
         </div>
       </div>
     </div>
 
-    <div>
-      <p>{{ product.category }}</p>
-      <h3>{{ product.name }}</h3>
-      <span>${{ product.price }}.00</span>
+    <div class="flex flex-col items-center justify-center py-4">
+      <p class="capitalize text-custom-btn-gray">{{ product.category }}</p>
+      <h3 class="group-hover:text-custom-blue transition-all duration-350 capitalize font-bold my-2">{{ product.name }}</h3>
+      <span class="capitalize text-custom-btn-gray">${{ product.price }}.00</span>
     </div>
   </section>
 </template>
